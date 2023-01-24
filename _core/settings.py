9ed5071724798024b74f45f8efc4fe9509974813
Marchi8 <luandeoliveirasoares@gmail.com>
2023-01-24 11:11:48 -0300

    chore: .env config

--- _core/settings.py
+++ _core/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
 
+dotenv.load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8a1$oedz2gp-x=&f)g*8!0(y*q629xm&smf5-bk(vq5tbj7$)4"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
